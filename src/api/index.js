652f0b784b8fdb05e8a035f15c8ffc4380b73d29
Aleilton Chavenco <aleilton@gmail.com>
2022-04-10 22:30:38 -0300

    ajustes de config

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const URL = 'http://localhost:3001';
 // const URL = 'http://192.168.1.76:3001';
-const URL = 'https://mysql-deploy-ton.herokuapp.com/';
+const URL = 'https://mysql-deploy-ton.herokuapp.com';
 
 export const fetchAllProducts = (token) => axios.get(`${URL}/products`,
   { headers: { authorization: `${token}` } });
